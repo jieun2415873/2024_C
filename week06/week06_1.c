8d96878302744454add533281a8cd5d27439d0d6
jieun2415873 <wldms@sookmyung.ac.kr>
2024-10-14 13:03:10 +0900

    week06_1

---
+++ week06/week06_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int number;
+	
+	printf("Enter an integer :");
+	scanf("%i", &number);
+	
+	if (number > 0) {
+		printf("This is positive number\n");
+	} else if (number < 0) {
+		printf("This is negative number\n");
+	} else {
+		printf("This is 0\n");
+	}
+	
+	return 0;
+}
