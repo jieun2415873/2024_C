6b46bb779bc07b36ae0db4a309a4e3953a1266c4
jieun2415873 <wldms@sookmyung.ac.kr>
2024-10-14 13:16:15 +0900

    week06_3

---
+++ week06/week06_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+	int num = 0;
+	
+	printf("Input a string :");
+	
+	while ((c = getchar()) != '\n') {
+		if (c >= '0' && c <= '9') {
+			num++;
+		}
+	}
+	
+	printf("The number of digits is %d\n", num);
+	
+	return 0;
+}
