2eb927d003ddfa9e85bf605a04403f839159a884
jieun2415873 <wldms@sookmyung.ac.kr>
2024-10-16 13:58:32 +0900

    week07_3

---
+++ week07/week07_3.c
@@ -0,0 +1,27 @@
+/*#include <stdio.h>
+
+void square ( int a )
+{
+	a = a*a;
+}
+
+int main()
+{
+	int a = 2;
+	square(a);
+	printf("a=%i\n", a);
+}*/
+
+#include <stdio.h>
+
+int square( int a )
+{
+	return(a*a);
+}
+
+int main()
+{
+	int a = 2;
+	a = square(a);
+	printf("a = %i\n", a);
+}
