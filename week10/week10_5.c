b7feea3b06aaf724b0288374981e9a06b1db56bb
jieun2415873 <wldms@sookmyung.ac.kr>
2024-11-06 13:52:31 +0900

    week10_5

---
+++ week10/week10_5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define ROWS       3
+#define COLS       3
+
+void addMatrix(int A[ROWS][COLS], int B[ROWS][COLS], int C[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            C[i][j] = A[i][j] + B[i][j]; 
+        }
+    }
+}
+
+void printMatrix(int matrix[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");  
+    }
+}
+
+int main(void)
+{
+	int A[ROWS][COLS]={
+	    {2,3,0},
+	    {8,9,1},
+	    {7,0,5}};
+	int B[ROWS][COLS]={
+	    {1,0,0},
+	    {0,1,0},
+	    {0,0,1}};
+	int C[ROWS][COLS];
+	
+	addMatrix(A,B,C);
+	printMatrix(C);
+	
+	return 0;
+}
